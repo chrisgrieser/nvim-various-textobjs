363dbb7bd7b546a90359e17a6700fa07d2c2f4fe
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-03-31 21:06:03 +0200

    feat!: dot-repeatability. See the README (fix #7)

--- lua/various-textobjs.lua
+++ lua/various-textobjs.lua
@@ -61,17 +61,22 @@ local function setupKeymaps()
 	local keymap = vim.keymap.set
 	for objName, map in pairs(innerOuterMaps) do
 		local name = " " .. objName .. " textobj"
-		keymap({ "o", "x" }, "a" .. map, function() M[objName](false) end, { desc = "outer" .. name })
-		keymap({ "o", "x" }, "i" .. map, function() M[objName](true) end, { desc = "inner" .. name })
+		keymap({ "o", "x" }, "a" .. map, "<cmd>lua require('various-textobjs')." .. objName .. "(true)<CR>", { desc = "outer" .. name })
+		keymap({ "o", "x" }, "i" .. map, "<cmd>lua require('various-textobjs')." .. objName .. "(false)<CR>", { desc = "inner" .. name })
 	end
 	for objName, map in pairs(oneMaps) do
-		keymap({ "o", "x" }, map, M[objName], { desc = objName .. " textobj" })
+		keymap(
+			{ "o", "x" },
+			map,
+			"<cmd>lua require('various-textobjs')." .. objName .. "()<CR>",
+			{ desc = objName .. " textobj" }
+		)
 	end
 	-- stylua: ignore start
-	keymap( { "o", "x" }, "ii" , function() M.indentation(true, true) end, { desc = "inner-inner indentation textobj" })
-	keymap( { "o", "x" }, "ai" , function() M.indentation(false, true) end, { desc = "outer-inner indentation textobj" })
-	keymap( { "o", "x" }, "iI" , function() M.indentation(true, true) end, { desc = "inner-inner indentation textobj" })
-	keymap( { "o", "x" }, "aI" , function() M.indentation(false, false) end, { desc = "outer-outer indentation textobj" })
+	keymap( { "o", "x" }, "ii" , "<cmd>lua require('various-textobjs').indentation(true, true)<CR>", { desc = "inner-inner indentation textobj" })
+	keymap( { "o", "x" }, "ai" , "<cmd>lua require('various-textobjs').indentation(false, true)<CR>", { desc = "outer-inner indentation textobj" })
+	keymap( { "o", "x" }, "iI" , "<cmd>lua require('various-textobjs').indentation(true, true)<CR>", { desc = "inner-inner indentation textobj" })
+	keymap( { "o", "x" }, "aI" , "<cmd>lua require('various-textobjs').indentation(false, false)<CR>", { desc = "outer-outer indentation textobj" })
 
 	vim.api.nvim_create_augroup("VariousTextobjs", {})
 	for _, textobj in pairs(ftMaps) do
@@ -564,7 +569,7 @@ function M.cssSelector(inner)
 end
 
 ---HTML/XML Attribute Textobj
----@param inner boolean inner selector is only the value of the attribute inside the quotation marks. 
+---@param inner boolean inner selector is only the value of the attribute inside the quotation marks.
 function M.htmlAttribute(inner)
 	local pattern = '(%w+=").-(")'
 	searchTextobj(pattern, inner)
