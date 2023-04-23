6d2f1d3fd78560087da77286500a4adb4692d24a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-04-23 06:00:43 +0200

    fix: various edge cases for column textobj

--- lua/various-textobjs/blockwise-textobjs.lua
+++ lua/various-textobjs/blockwise-textobjs.lua
@@ -7,28 +7,27 @@ local u = require("various-textobjs.utils")
 ---Column Textobj (blockwise down until indent or shorter line)
 function M.column()
 	local lastLnum = fn.line("$")
-	local nextLnum, cursorCol = unpack(u.getCursor(0))
+	local startRow = u.getCursor(0)[1]
+	local trueCursorCol = fn.virtcol(".") -- virtcol accurately accounts for tabs as indentation
 	local extraColumns = vim.v.count1 - 1 -- has to be done before running the other :normal commands, since they change v:count
-
-	-- get accurate cursorCol (account for tabs/spaces properly)
-	if not bo.expandtab then
-		local indentLevel = (fn.indent(".") / bo.tabstop) ---@diagnostic disable-line: param-type-mismatch
-		cursorCol = cursorCol + (indentLevel * (bo.tabstop - 1))
-	end
+	local nextLnum = startRow
 
 	repeat
 		nextLnum = nextLnum + 1
-		if nextLnum > lastLnum then break end -- break here, since after end of file, getline will fail
-		local trueLineLength = #u.getline(nextLnum):gsub("\t", string.rep(" ", bo.tabstop)) ---@diagnostic disable-line: undefined-field
-		local shorterLine = trueLineLength < cursorCol
-		local hitsIndent = cursorCol < fn.indent(nextLnum)
+		if nextLnum > lastLnum then break end
+		local trueLineLength = #u.getline(nextLnum):gsub("\t", string.rep(" ", bo.tabstop))
+		local shorterLine = trueLineLength <= trueCursorCol
+		local hitsIndent = trueCursorCol <= fn.indent(nextLnum)
 	until hitsIndent or shorterLine
-	nextLnum = nextLnum - 1
+	local linesDown = nextLnum - 1 - startRow
 
 	-- start visual block mode
 	if not (fn.mode() == "CTRL-V") then vim.cmd.execute([["normal! \<C-v>"]]) end
 
-	u.normal(nextLnum .. "G")
+	-- set position
+	-- not using `setCursor`, since its column-positions are messed up by tab indentation
+	-- not using `G` to go down lines, since affected by `opt.startofline`
+	if linesDown > 0 then u.normal(tostring(linesDown) .. "j") end
 	if extraColumns > 0 then u.normal(tostring(extraColumns) .. "l") end
 end
 
