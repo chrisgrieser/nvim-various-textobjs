eaa2eba379202ce6ab8cd7b1c41e647227d2bb91
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-12-04 19:45:43 +0100

    chore: add deprecation info for `charwise.urlPattern`

---
+++ lua/various-textobjs/charwise-textobjs.lua
@@ -0,0 +1,16 @@
+local M = {}
+--------------------------------------------------------------------------------
+
+-- DEPRECATION (2024-12-04)
+setmetatable(M, {
+	__index = function(_, key)
+		if key == "urlPattern" then
+			local msg =
+				'`require("various-textobjs.charwise-textobjs").urlPattern` is deprecated. Just use this pattern instead: "%l%l%l-://%S+"'
+			require("various-textobjs.utils").warn(msg)
+		end
+	end,
+})
+
+--------------------------------------------------------------------------------
+return M
