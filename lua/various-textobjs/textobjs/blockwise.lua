44267b1d95cac64e6dc955a6b4215924751e8b27
Ivo Maceira <ivomaceira@gmail.com>
2025-05-03 19:12:42 +0200

    refactor: combine column up and down functions

--- lua/various-textobjs/textobjs/blockwise.lua
+++ lua/various-textobjs/textobjs/blockwise.lua
@@ -2,8 +2,13 @@ local M = {}
 local u = require("various-textobjs.utils")
 --------------------------------------------------------------------------------
 
----Column Textobj (blockwise down until indent or shorter line)
-function M.column()
+---Column Textobj (blockwise up or down until indent or shorter line)
+---@param direction number Direction: +1 for "down" (default), -1 for "up"
+function M.column(direction)
+
+	direction = direction == -1 and direction or 1
+	local key = direction == -1 and "k" or "j"
+
 	local lastLnum = vim.api.nvim_buf_line_count(0)
 	local startRow = vim.api.nvim_win_get_cursor(0)[1]
 	local trueCursorCol = vim.fn.virtcol(".") -- virtcol accurately accounts for tabs as indentation
@@ -12,13 +17,13 @@ function M.column()
 	local nextLnum = startRow
 
 	repeat
-		nextLnum = nextLnum + 1
-		if nextLnum > lastLnum then break end
+		nextLnum = nextLnum + direction
+		if nextLnum > lastLnum or nextLnum < 0 then break end
 		local trueLineLength = #u.getline(nextLnum):gsub("\t", string.rep(" ", vim.bo.tabstop))
 		local shorterLine = trueLineLength <= trueCursorCol
 		local hitsIndent = trueCursorCol <= vim.fn.indent(nextLnum)
 	until hitsIndent or shorterLine
-	local linesDown = nextLnum - 1 - startRow
+	local linesToMove = direction*(nextLnum - startRow) - 1
 
 	-- SET POSITION
 	u.saveJumpToJumplist()
@@ -28,35 +33,7 @@ function M.column()
 
 	-- not using `setCursor`, since its column-positions are messed up by tab indentation
 	-- not using `G` to go down lines, since affected by `opt.startofline`
-	if linesDown > 0 then u.normal(tostring(linesDown) .. "j") end
-	if extraColumns > 0 then u.normal(tostring(extraColumns) .. "l") end
-end
-
----Column Textobj (blockwise up until indent or shorter line)
-function M.column_up()
-	local startRow = vim.api.nvim_win_get_cursor(0)[1]
-	local trueCursorCol = vim.fn.virtcol(".") -- virtcol accurately accounts for tabs as indentation
-	local extraColumns = vim.v.count1 - 1 -- before running other :normal commands, since they change v:count
-
-	local nextLnum = startRow
-
-	repeat
-		nextLnum = nextLnum - 1
-		if nextLnum < 0 then break end
-		local trueLineLength = #u.getline(nextLnum):gsub("\t", string.rep(" ", vim.bo.tabstop))
-		local shorterLine = trueLineLength <= trueCursorCol
-		local hitsIndent = trueCursorCol <= vim.fn.indent(nextLnum)
-	until hitsIndent or shorterLine
-	local linesUp = startRow - nextLnum - 1
-
-	-- SET POSITION
-	u.saveJumpToJumplist()
-
-	-- start visual block mode ( requires special character `^V`)
-	if not (vim.fn.mode() == "") then vim.cmd.execute([["normal! \<C-v>"]]) end
-
-	-- not using `setCursor`, since its column-positions are messed up by tab indentation
-	if linesUp > 0 then u.normal(tostring(linesUp) .. "k") end
+	if linesToMove > 0 then u.normal(tostring(linesToMove) .. key) end
 	if extraColumns > 0 then u.normal(tostring(extraColumns) .. "l") end
 end
 
