4ada226fb11d83fb489e4ff4b690db3111d0bd07
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-11-30 17:24:14 +0100

    refactor: deprecation function

--- lua/various-textobjs/init.lua
+++ lua/various-textobjs/init.lua
@@ -4,77 +4,81 @@ local M = {}
 ---@param userConfig? VariousTextobjs.Config
 function M.setup(userConfig) require("various-textobjs.config.config").setup(userConfig) end
 
+-- DEPRECATION (2025-11-30)
+---@param key string
+---@return boolean -- key was deprecated
+local function deprecations(key)
+	local warning = nil
+	if key == "mdFencedCodeBlock" then
+		warning = "The `mdFencedCodeBlock` textobj is deprecated. "
+			.. "Please use `nvim-treesitter-teextobjects`, create a file "
+			.. "`./queries/markdown/textobjects.scm` in your config dir with "
+			.. "the following content:\n\n"
+			.. "```\n"
+			.. "; extends\n"
+			.. "(fenced_code_block) @codeblock.outer\n"
+			.. "(code_fence_content) @codeblock.inner\n"
+			.. "```\n"
+			.. "Call the textobject via `:TSTextobjectSelect @codeblock.outer`"
+	elseif key == "mdLink" then
+		warning = "The `mdLink` textobj is deprecated. "
+			.. "Please use `nvim-treesitter-teextobjects`, create a file "
+			.. "`./queries/markdown_inline/textobjects.scm` in your config dir with "
+			.. "the following content:\n\n"
+			.. "```\n"
+			.. "; extends\n"
+			.. "(inline_link) @mdlink.outer\n"
+			.. "(link_text) @mdlink.inner\n"
+			.. "```\n"
+			.. "Call the textobject via `:TSTextobjectSelect @mdlink.outer`"
+	elseif key == "mdEmphasis" then
+		warning = "The `mdEmphasis` textobj is deprecated. "
+			.. "Please use `nvim-treesitter-teextobjects`, create a file "
+			.. "`./queries/markdown_inline/textobjects.scm` in your config dir with "
+			.. "the following content:\n\n"
+			.. "```\n"
+			.. "; extends\n"
+			.. "(emphasis) @emphasis.outer\n"
+			.. "(strong_emphasis) @emphasis.outer\n"
+			.. "```\n"
+			.. "Call the textobject via `:TSTextobjectSelect @emphasis.outer`"
+	elseif key == "cssSelector" then
+		warning = "The `cssSelector` textobj is deprecated. "
+			.. "Please use `nvim-treesitter-teextobjects`, create a file "
+			.. "`./queries/css/textobjects.scm` in your config dir with "
+			.. "the following content:\n\n"
+			.. "```\n"
+			.. "; extends\n"
+			.. '(class_selector "." @selector.outer (class_name) @selector.inner @selector.outer)\n'
+			.. "```\n"
+			.. "Call the textobject via `:TSTextobjectSelect @selector.outer`"
+	elseif key == "shellPipe" then
+		warning = "The `shellPipe` textobj is deprecated. "
+			.. "Please use `nvim-treesitter-teextobjects`, create a file "
+			.. "`./queries/{zsh,bash}/textobjects.scm` in your config dir with "
+			.. "the following content:\n\n"
+			.. "```\n"
+			.. "; extends\n"
+			.. '(pipeline (command) @pipeline.inner @pipeline.outer "|" @pipeline.outer)\n'
+			.. "```\n"
+			.. "Call the textobject via `:TSTextobjectSelect @pipeline.outer`"
+	elseif key == "htmlAttribute" then
+		warning = "The `htmlAttribute` textobj is deprecated. "
+			.. "Please use the existing `@attribute.outer` textobj from "
+			.. "`nvim-treesitter-teextobjects`."
+	end
+	if warning then
+		require("various-textobjs.utils").warn(warning)
+		return true
+	end
+	return false
+end
+
 -- redirect calls to this module to the respective submodules
 setmetatable(M, {
 	__index = function(_, key)
 		return function(...)
-			local warn = require("various-textobjs.utils").warn
-			-----------------------------------------------------------------------
-			-- DEPRECATION (2025-11-30)
-			local warning
-			if key == "mdFencedCodeBlock" then
-				warning = "The `mdFencedCodeBlock` textobj is deprecated. "
-					.. "Please use `nvim-treesitter-teextobjects`, create a file "
-					.. "`./queries/markdown/textobjects.scm` in your config dir with "
-					.. "the following content:\n\n"
-					.. "```\n"
-					.. "; extends\n"
-					.. "(fenced_code_block) @codeblock.outer\n"
-					.. "(code_fence_content) @codeblock.inner\n"
-					.. "```\n"
-					.. "Call the textobject via `:TSTextobjectSelect @codeblock.outer`"
-			elseif key == "mdLink" then
-				warning = "The `mdLink` textobj is deprecated. "
-					.. "Please use `nvim-treesitter-teextobjects`, create a file "
-					.. "`./queries/markdown_inline/textobjects.scm` in your config dir with "
-					.. "the following content:\n\n"
-					.. "```\n"
-					.. "; extends\n"
-					.. "(inline_link) @mdlink.outer\n"
-					.. "(link_text) @mdlink.inner\n"
-					.. "```\n"
-					.. "Call the textobject via `:TSTextobjectSelect @mdlink.outer`"
-			elseif key == "mdEmphasis" then
-				warning = "The `mdEmphasis` textobj is deprecated. "
-					.. "Please use `nvim-treesitter-teextobjects`, create a file "
-					.. "`./queries/markdown_inline/textobjects.scm` in your config dir with "
-					.. "the following content:\n\n"
-					.. "```\n"
-					.. "; extends\n"
-					.. "(emphasis) @emphasis.outer\n"
-					.. "(strong_emphasis) @emphasis.outer\n"
-					.. "```\n"
-					.. "Call the textobject via `:TSTextobjectSelect @emphasis.outer`"
-			elseif key == "cssSelector" then
-				warning = "The `cssSelector` textobj is deprecated. "
-					.. "Please use `nvim-treesitter-teextobjects`, create a file "
-					.. "`./queries/css/textobjects.scm` in your config dir with "
-					.. "the following content:\n\n"
-					.. "```\n"
-					.. "; extends\n"
-					.. '(class_selector "." @selector.outer (class_name) @selector.inner @selector.outer)\n'
-					.. "```\n"
-					.. "Call the textobject via `:TSTextobjectSelect @selector.outer`"
-			elseif key == "shellPipe" then
-				warning = "The `shellPipe` textobj is deprecated. "
-					.. "Please use `nvim-treesitter-teextobjects`, create a file "
-					.. "`./queries/{zsh,bash}/textobjects.scm` in your config dir with "
-					.. "the following content:\n\n"
-					.. "```\n"
-					.. "; extends\n"
-					.. '(pipeline (command) @pipeline.inner @pipeline.outer "|" @pipeline.outer)\n'
-					.. "```\n"
-					.. "Call the textobject via `:TSTextobjectSelect @pipeline.outer`"
-			elseif key == "htmlAttribute" then
-				warning = "The `htmlAttribute` textobj is deprecated. "
-					.. "Please use the existing `@attribute.outer` textobj from "
-					.. "`nvim-treesitter-teextobjects`."
-			end
-			if warning then
-				warn(warning)
-				return function() end -- empty function to prevent error
-			end
-			-----------------------------------------------------------------------
+			if deprecations(key) then return end
 
 			local linewiseObjs = vim.tbl_keys(require("various-textobjs.textobjs.linewise"))
 			local charwiseObjs = vim.tbl_keys(require("various-textobjs.textobjs.charwise"))
@@ -92,7 +96,7 @@ setmetatable(M, {
 			else
 				local msg = ("There is no text object called `%s`.\n\n"):format(key)
 					.. "Make sure it exists in the list of text objects, and that you haven't misspelled it."
-				warn(msg)
+				require("various-textobjs.utils").warn(msg)
 			end
 		end
 	end,
